59c15003b309ab93a1f751bab074b62ddef13eae
S-hre29ya07 <singh.shreya2907@gmail.com>
2022-09-30 12:04:37 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username: string = '';
   title = 'Fyle-project';
+  userSearchHandler(username: string) {
+    this.username = username;
+  }
 }
